e29d25c28f73af804ebab9356bd9590061c6818e
krishnakoushik9 <legionkoushik3@gmail.com>
2025-07-09 23:04:01 +0530

    Second Correct iteration of Binary seach code algorithm

--- FirstJavaProject/src/krishnayt/BinarySearch.java
+++ FirstJavaProject/src/krishnayt/BinarySearch.java
@@ -2,15 +2,25 @@ package krishnayt;
 
 public class BinarySearch {
     public static void main(String[] args) {
-        int[] arr = {2,12,24,42,56,66,68,71,90,91};
-        binaryelement(arr,0,arr.length,0,68);
-
+        int[] arr = {-18,-7,0,1,4,12,23,35,44,58,72,91};
+        int target = 23;
+        int ans = binarySearch(arr,target);
+        System.out.println(ans);
     }
-
-    static int binaryelement(int[] arr, int s, int e, int m,int target) {
-
-        s = 0;
-        e = arr.length;
-        m = (s + e)/2;
+    static int binarySearch(int[] arr,int target){
+        int start = 0;
+        int end = arr.length - 1;
+        while(start <= end){
+            int mid = start + (end - start)/ 2;
+            if (target < arr[mid]){
+                end = mid-1;
+            }
+            if (target > arr[mid]){
+                start = mid + 1;
+            }
+            else{
+                return mid;
+            }
+        }return -1;
     }
 }
