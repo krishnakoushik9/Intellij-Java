8dfc6c528222c4a27ac1dc246dd659a496be85d5
krishnakoushik9 <legionkoushik3@gmail.com>
2025-07-09 23:12:27 +0530

    I just Practiced it again

--- FirstJavaProject/src/krishnayt/BinarySearch.java
+++ FirstJavaProject/src/krishnayt/BinarySearch.java
@@ -11,20 +11,20 @@ public class BinarySearch {
         System.out.println("Time taken (ms): " + (end - start) / 1_000_000.0);
     }
     static int binarySearch(int[] arr,int target){
-    int start = 0;
-    int end = arr.length - 1;
-    while(start <= end) {
-        int mid = start + (end - start) / 2;
-        if (target < arr[mid]) {
-            end = mid - 1;
-        }
-        if (target > arr[mid]) {
-            start = mid + 1;
-        }
-        else
-        {
-            return mid;
-        }
-    }return -1;
+        int start = 0;
+        int end = arr.length - 1;
+        while(start <= end){
+            int mid = start + (end - start)/2;
+            if(target > arr[mid]){
+                start = mid + 1;
+            }
+            if(target < arr[mid]){
+                end = mid -1;
+            }
+            else {
+                return mid;
+            }
+        }return -1;
     }
+
 }
